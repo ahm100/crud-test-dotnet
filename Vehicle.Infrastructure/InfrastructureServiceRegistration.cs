418f02e1f67dfef35a9418ad9b247424ea5373be
KOOROSH130190\a.sadegh <ahmad.sadegh@gmail.com>
2025-01-09 01:45:58 +0330

    Microsoft.AspNetCore.Authentication.JwtBearer

--- Vehicle.Infrastructure/InfrastructureServiceRegistration.cs
+++ Vehicle.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +21,15 @@ namespace Vehicle.Infrastructure
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            // Register application services services.AddScoped<IUserService, UserService>();
+
+            services.AddAuthentication(options => { options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; })
+            .AddJwtBearer(options => { options.TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = true, ValidateAudience = true, ValidateLifetime = true, ValidateIssuerSigningKey = true, ValidIssuer = configuration["Jwt:Issuer"], ValidAudience = configuration["Jwt:Issuer"], IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])) }; });
+
+            services.AddControllersWithViews();
 
             services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             
