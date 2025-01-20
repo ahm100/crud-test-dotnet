a96ce75f0598c2c6f4aa3fe260c3922d648df0f7
Ahmad Sadegh <hp1@ptsolutions.org>
2025-01-20 16:43:35 +0330

    add commeit

--- Vehicle.API/SharedLocalizer.cs
+++ Vehicle.API/SharedLocalizer.cs
@@ -14,6 +14,20 @@ namespace Vehicle.API
         }
 
         public string this[string key] => _localizer[key];
+        /*
+         * public string: This specifies that the indexer returns a string and is publicly accessible.
+
+this[string key]: This defines the indexer. The this keyword indicates it's an indexer, and [string key] specifies that it takes a string as a parameter (the key).
+
+=> _localizer[key];: This is the expression-bodied member syntax. It means that when the indexer is accessed with a key, it returns the value from the _localizer using that key.
+
+So, when you use localizer["Hello World"], it uses the indexer to get the localized string associated with the key "Hello World".
+
+
+        we can use instead :
+        public string GetString(string key) { return _localizer[key]; }
+        and call (_localizer.GetString("Hello World")
+         */
     }
 
 }
